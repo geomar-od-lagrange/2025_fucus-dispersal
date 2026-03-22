9aa177f06dce3515db776b6d07ca1bde7a606e55
Willi Rath <wrath@geomar.de>
2026-03-22 14:27:50 +0100

    Restructure

--- job_scripts/Fucus_BSH_dispersal.sh
+++ job_scripts/Fucus_BSH_dispersal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=19h1d0s087Fucus_dispersal
+#SBATCH --job-name=Fucus_dispersal
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=120G
@@ -14,6 +14,7 @@ module load singularity/3.11.5
 # to get the image (need to be on a partition which has internet access --> data), run
 # $ singularity pull --disable-cache --dir "${PWD}" docker://quay.io/willirath/parcels-container:2024.10.07-7af7fd0
 
+base_path=/gxfs_work/geomar/smomw597/2025_Fucus
 release_year=2019
 first_release_month=1
 first_release_day=1
@@ -21,15 +22,12 @@ last_release_month=6
 last_release_day=30
 release_depth=0
 relative_particle_speed=0.87
-# make sure the output exists
-mkdir -p notebooks_executed/
-# run for single notebook and put into background
+# make sure the output dir exists
 mkdir -p notebooks_executed/TrajectoryCalc/${release_year}/
-mkdir -p output/Trajectories/${release_year}/
 srun --ntasks=1 --exclusive singularity run -B /sfs -B /gxfs_work -B $PWD:/work --pwd /work parcels-container_2024.10.07-7af7fd0.sif bash -c \
 ". /opt/conda/etc/profile.d/conda.sh && conda activate base \
 && papermill --cwd notebooks/ \
-    notebooks/FucusDispersal.ipynb \
+    notebooks/010_FucusDispersal.ipynb \
     notebooks_executed/TrajectoryCalc/${release_year}/Fucus_y${release_year}_m${first_release_month}-${last_release_month}_d${release_depth}_relspeed${relative_particle_speed}.ipynb \
     -p release_year ${release_year} \
     -p first_release_month ${first_release_month} \
@@ -44,12 +42,9 @@ srun --ntasks=1 --exclusive singularity run -B /sfs -B /gxfs_work -B $PWD:/work 
     -p repeated_release True \
     -p repeated_release_dt_days 7 \
     -p relative_particle_speed ${relative_particle_speed} \
+    -p base_path ${base_path} \
     -p is_papermill True \
-    -k python" &
-    
-
-# wait till background task is done
-wait
+    -k python"
 
 # print resource infos
 jobinfo
